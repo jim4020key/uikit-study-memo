7dd0c9160e7c05afcb0381c0b41428d00084559c
JIMIN KIM <jim4020key@gmail.com>
2022-07-11 00:58:38 +0900

    Add MemoData model

---
+++ uikit-study/MemoData.swift
@@ -0,0 +1,16 @@
+//
+//  MemoData.swift
+//  uikit-study
+//
+//  Created by kimjimin on 2022/07/11.
+//
+
+import UIKit
+
+class MemoData {
+    var memoIdx : Int?
+    var title : String?
+    var contents : String?
+    var image : UIImage?
+    var regdate : Date?
+}
