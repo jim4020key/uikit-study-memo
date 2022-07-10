3df23483fd19e3ec4661b5187286bd0e780c8b16
JIMIN KIM <jim4020key@gmail.com>
2022-07-11 00:32:05 +0900

    Add custom classes

---
+++ uikit-study/MemoCell.swift
@@ -0,0 +1,23 @@
+//
+//  MemoCell.swift
+//  uikit-study
+//
+//  Created by kimjimin on 2022/07/11.
+//
+
+import UIKit
+
+class MemoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
