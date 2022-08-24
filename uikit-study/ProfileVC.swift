ca70479668dbc1711b0777ea1f44f93ee190eac3
JIMIN KIM <jim4020key@gmail.com>
2022-08-25 02:43:49 +0900

    Implement Login

--- uikit-study/ProfileVC.swift
+++ uikit-study/ProfileVC.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class ProfileVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let userInfo = UserInfoManager()
     let profileImage = UIImageView()
     let tableView = UITableView()
     
@@ -19,7 +20,7 @@ class ProfileVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         let backButton = UIBarButtonItem(title: "닫기", style: .plain, target: self, action: #selector(close(_:)))
         self.navigationItem.leftBarButtonItem = backButton
         
-        let image = UIImage(named: "account.jpg")
+        let image = self.userInfo.profile
         self.profileImage.image = image
         self.profileImage.frame.size = CGSize(width: 100, height: 100)
         self.profileImage.center = CGPoint(x: self.view.frame.width / 2, y: 270)
@@ -43,6 +44,8 @@ class ProfileVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         self.view.addSubview(bgImage)
         self.view.bringSubviewToFront(self.tableView)
         self.view.bringSubviewToFront(self.profileImage)
+        
+        self.drawButton()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -58,10 +61,10 @@ class ProfileVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         switch indexPath.row {
         case 0:
             cell.textLabel?.text = "이름"
-            cell.detailTextLabel?.text = "JIMIN KIM"
+            cell.detailTextLabel?.text = self.userInfo.name ?? "Login please"
         case 1:
             cell.textLabel?.text = "계정"
-            cell.detailTextLabel?.text = "jim4020key@gmail.com"
+            cell.detailTextLabel?.text = self.userInfo.account ?? "Login please"
         default:
             ()
         }
@@ -69,7 +72,81 @@ class ProfileVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if self.userInfo.isLogin == false {
+            self.doLogin(self.tableView)
+        }
+    }
+    
+    func drawButton() {
+        let view = UIView()
+        view.frame.size.width = self.view.frame.width
+        view.frame.size.height = 43.5
+        view.frame.origin.x = 0
+        view.frame.origin.y = self.tableView.frame.origin.y + self.tableView.frame.height
+        view.backgroundColor = UIColor(red: 0.98, green: 0.98, blue: 0.98, alpha: 1.0)
+        self.view.addSubview(view)
+        
+        let button = UIButton(type: .system)
+        button.frame.size.width = 100
+        button.frame.size.height = 30
+        button.center.x = view.frame.size.width / 2
+        button.center.y = view.frame.size.height / 2
+        
+        if self.userInfo.isLogin == true {
+            button.setTitle("로그아웃", for: .normal)
+            button.addTarget(self, action: #selector(doLogout(_:)), for: .touchUpInside)
+        } else {
+            button.setTitle("로그인", for: .normal)
+            button.addTarget(self, action: #selector(doLogin(_:)), for: .touchUpInside)
+        }
+        view.addSubview(button)
+    }
+    
     @objc func close(_ sender: Any) {
         self.presentingViewController?.dismiss(animated: true)
+    }
+    
+    @objc func doLogin(_ sender: Any) {
+        let loginAlert = UIAlertController(title: "LOGIN", message: nil, preferredStyle: .alert)
+        loginAlert.addTextField() { (tf) in
+            tf.placeholder = "Your Account"
+        }
+        loginAlert.addTextField() { (tf) in
+            tf.placeholder = "Password"
+            tf.isSecureTextEntry = true
+        }
+        loginAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        loginAlert.addAction(UIAlertAction(title: "Login", style: .destructive) { (_) in
+            let account = loginAlert.textFields?[0].text ?? ""
+            let password = loginAlert.textFields?[1].text ?? ""
+            
+            if self.userInfo.login(account: account, password: password) {
+                self.tableView.reloadData()
+                self.profileImage.image = self.userInfo.profile
+                self.drawButton()
+            } else {
+                let message = "로그인에 실패하였습니다."
+                let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .cancel))
+                self.present(alert, animated: false)
+            }
+        })
+        self.present(loginAlert, animated: false)
+    }
+    
+    @objc func doLogout(_ sender: Any) {
+        let message = "로그아웃하시겠습니까?"
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "취소", style: .cancel))
+        alert.addAction(UIAlertAction(title: "확인", style: .destructive) { (_) in
+            if self.userInfo.logout() {
+                self.tableView.reloadData()
+                self.profileImage.image = self.userInfo.profile
+                self.drawButton()
+            }
+        })
+        self.present(alert, animated: false)
     }
 }
