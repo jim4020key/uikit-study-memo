a86d9c95fd246e74cc81f5dc97421442a92079d1
JIMIN KIM <jim4020key@gmail.com>
2022-09-14 04:06:48 +0900

    Set storyboard for profile

---
+++ uikit-study/JoinVC.swift
@@ -0,0 +1,66 @@
+//
+//  JoinVC.swift
+//  uikit-study
+//
+//  Created by kimjimin on 2022/09/14.
+//
+
+import UIKit
+import Alamofire
+
+class JoinVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var profile: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func submit(_ sender: Any) {
+    }
+    
+    var fieldAccount: UITextField!
+    var fieldPassword: UITextField!
+    var fieldName: UITextField!
+    
+    override func viewDidLoad() {
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        
+        let textFieldFrame = CGRect(x: 20, y: 0, width: cell.bounds.width - 20, height: 37)
+        switch indexPath.row {
+        case 0:
+            self.fieldAccount = UITextField(frame: textFieldFrame)
+            self.fieldAccount.placeholder = "계정(이메일)"
+            self.fieldAccount.borderStyle = .none
+            self.fieldAccount.autocapitalizationType = .none
+            self.fieldAccount.font = UIFont.systemFont(ofSize: 14)
+            cell.addSubview(self.fieldAccount)
+        case 1:
+            self.fieldPassword = UITextField(frame: textFieldFrame)
+            self.fieldPassword.placeholder = "비밀번호"
+            self.fieldPassword.borderStyle = .none
+            self.fieldPassword.autocapitalizationType = .none
+            self.fieldPassword.font = UIFont.systemFont(ofSize: 14)
+            cell.addSubview(self.fieldPassword)
+        case 2:
+            self.fieldName = UITextField(frame: textFieldFrame)
+            self.fieldName.placeholder = "이름"
+            self.fieldName.borderStyle = .none
+            self.fieldName.autocapitalizationType = .none
+            self.fieldName.font = UIFont.systemFont(ofSize: 14)
+            cell.addSubview(self.fieldName)
+        default:
+            ()
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 40
+    }
+}
