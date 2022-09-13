7cfbe65da6d6480fe5e5198266be05fcc478ad97
JIMIN KIM <jim4020key@gmail.com>
2022-09-14 03:19:23 +0900

    Add function for utility

--- uikit-study/Utils.swift
+++ uikit-study/Utils.swift
@@ -12,7 +12,19 @@ extension UIViewController {
     var tutorialSB: UIStoryboard {
         return UIStoryboard(name: "Tutorial", bundle: Bundle.main)
     }
+    
     func instanceTutorialVC(name: String) -> UIViewController? {
         return self.tutorialSB.instantiateViewController(withIdentifier: name)
+    }
+    
+    func alert(_ message: String, completion: (()->Void)? = nil) {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "확인", style: .cancel) { (_) in
+                completion?()
+            }
+            alert.addAction(okAction)
+            self.present(alert, animated: false)
+        }
     }
 }
